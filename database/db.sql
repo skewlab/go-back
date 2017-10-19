73e4307b123d621668dd3207b694dd807ab34425
fippli <filip.carl.johansson@gmail.com>
2017-10-19 19:55:05 +0200

    Modyfied the database sql file.

--- database/db.sql
+++ database/db.sql
@@ -17,17 +17,86 @@ DROP TABLE Users;
 
 CREATE TABLE Users (
   ID uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-  USERNAME text NOT NULL,
+  EMAIL text NOT NULL,
   PASSWORD text NOT NULL
 );
 
-/*
-Comments:
-Insert query:
-  INSERT INTO
-  Users ( name, password )
-  VALUES ( 'admin', crypt( 'password', gen_salt( 'bf', 8 ) ) );
-*/
+--
+-- Comments:
+-- Insert query:
+-- 	INSERT INTO
+-- 	Users ( name, password )
+-- 	VALUES ( 'admin', crypt( 'password', gen_salt( 'bf', 8 ) ) );
+--
+
+-- --------------------------------------------------------
+
+--
+-- User profile first name
+--
+
+CREATE TABLE Alias (
+	USERID uuid,
+	ALIAS varchar(255),
+	PUBLIC boolean
+);
+
+--
+-- Comments:
+--
+--
+
+-- --------------------------------------------------------
+
+--
+-- User profile email
+--
+
+CREATE TABLE Email (
+	USERID uuid,
+	EMAIL varchar(255),
+	PUBLIC boolean
+);
+
+--
+-- Comments:
+-- These are optional additional email
+-- Primary email is connected to the user in users table
+--
+
+-- --------------------------------------------------------
+
+--
+-- User profile birth date
+--
+
+CREATE TABLE UserBirthdate (
+	USERID uuid,
+	BIRTHDATE timestamp,
+	PUBLIC boolean
+);
+
+--
+-- Comments:
+-- Optional
+--
+
+-- --------------------------------------------------------
+
+--
+-- User profile picture
+--
+
+CREATE TABLE UserAvatar (
+	USERID uuid,
+	IMGPATH text,
+	PUBLIC boolean
+);
+
+--
+-- Comments:
+-- 
+--
 
 -- --------------------------------------------------------
 
@@ -44,5 +113,10 @@ CREATE TABLE Article (
   DATE_CREATED timestamp,
   DATE_UPDATED timestamp
 );
+
+--
+-- Comments:
+--
+--
 
 -- --------------------------------------------------------
