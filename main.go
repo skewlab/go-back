b548f6cc12ac77ab6fefe0e034ddb0a9147dbafa
Filip <filip.carl.johansson@gmail.com>
2017-10-21 03:51:55 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	// "net/http"
 	"encoding/json"
 	"github.com/labstack/echo"
 	"./api"
@@ -23,14 +21,9 @@ func main() {
 	configFile := util.ReadFile( "config.json" )
 	json.Unmarshal( []byte( configFile ), &config )
 
-	fmt.Printf( "\n > Starting server on localhost%v\n", config.Port )
-
-	// fs := http.FileServer( http.Dir( "static" ) )
-	// http.Handle( "/", fs )
 	e := echo.New()
 	api.Module( e )
 
-	// http.ListenAndServe( config.Port, nil )
-	e.Start(":8000")
+	e.Start( config.Port )
 
 }
