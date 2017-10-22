0867fb68aedad113c5a7befccce1492a8f02aa79
fippli <filip.carl.johansson@gmail.com>
2017-10-22 20:46:41 +0200

    User connection / friend requests functionality

---
+++ api/user-connections/accept.userconnections.go
@@ -0,0 +1,53 @@
+/*
+Author: Filip Johansson
+
+Description:
+Accept a user with connection.
+*/
+
+package userConnections
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+	"../../database"
+)
+
+type H map[string]interface{}
+
+/*
+TODO:
+Change this later on so that B is changed to the logged in users id.
+Only the userid of the connection requester should be posted.
+Authentication should also be required for accepting connections.
+*/
+func Accept() echo.HandlerFunc {
+
+	const (
+		query string = `
+			UPDATE connectionRequests
+			SET
+				Accepted = true
+			WHERE A = $1
+			AND   B = $2
+		`
+	)
+
+	var connectionRequest ConnectionRequest
+
+	return func( c echo.Context ) error {
+
+		c.Bind( &connectionRequest )
+
+		_, err := database.Connection().Query(
+			query,
+			connectionRequest.NewConnection,
+			connectionRequest.LoggedInUser )
+
+		if err != nil { return err }
+
+		return c.JSON( http.StatusCreated, H{ "message": "User connection requested" } )
+
+	}
+
+}
--- api/users/delete.users.go
+++ api/users/delete.users.go
@@ -1,11 +1,12 @@
 /*
 Author: Filip Johansson
+Email:  filip.carl.johansson@gmail.com
 
 Description:
-Delete user with delete request via /api/user
+Delete user with delete request via /api/users
 */
 
-package user
+package users
 
 import (
 	"net/http"
@@ -13,21 +14,26 @@ import (
 	"github.com/labstack/echo"
 )
 
+/*
+TODO:
+Authenticate user. Anyone should not be able to deleta a user.
+*/
 func Delete() echo.HandlerFunc {
+
+	const (
+		query string = `DELETE FROM Users WHERE id = $1`
+	)
 
 	return func( c echo.Context ) error {
 
 		id := c.Param( "id" )
-
-		var query string = `
-			DELETE FROM Users WHERE id = $1
-		`
 
 		_, err := database.Connection().Query( query, id )
 
 		if err != nil { return err }
 
 		return c.JSON( http.StatusCreated, H{ "message": "User deleted" } )
+		
 	}
 
 }
