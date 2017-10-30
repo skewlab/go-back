950bd7b3241e8f5a57a1238a05d5f5752452ab2e
fippli <filip.carl.johansson@gmail.com>
2017-10-30 11:24:40 +0100

    Testing back and front connections

--- api/api.go
+++ api/api.go
@@ -9,73 +9,73 @@ Include all the api endpoints.
 package api
 
 import (
-	"github.com/labstack/echo"
-	"./article"
-	"./users"
-	"./user-connections"
-	"./signin"
-	"./posts"
-	"./websockets"
-	"./ups"
+  "github.com/labstack/echo"
+  "./article"
+  "./users"
+  "./user-connections"
+  "./signin"
+  "./posts"
+  "./websockets"
+  "./ups"
 )
 
 func Module( e *echo.Echo ) {
 
-	//
-	// Users
-	//
-	e.GET("/api/users/:id", users.Get() )
-	e.POST("/api/users", users.Create() )
-	e.PUT("/api/users", users.Update() )
-	e.DELETE("/api/users/:id", users.Delete() )
-
-	//
-	// User connection
-	//
-
-	// NOTE: /:id will later be removed
-	//			 All connections should be returned
-	//			 /:id is now a replacement for logged in user id until sessions works.
-	e.GET("/api/user-connections/:id", userConnections.Get() )
-	e.POST("/api/user-connections", userConnections.Connect() )
-	e.PUT("/api/user-connections", userConnections.Accept() )
-
-	// NOTE: Change this to DELETE after sessions are working,
-	//			 change /remove to /:id
-	e.POST( "/api/user-connections/remove", userConnections.Disconnect() )
-	e.GET( "/api/my-contacts/:id", userConnections.MyContacts() )
-
-	//
-	// Articles
-	//
-	e.GET("/api/article/:id", article.Get() )
-	e.POST("/api/article", article.Post() )
-	e.PUT("/api/article", article.Put() )
-	e.DELETE("/api/article/:id", article.Delete() )
-
-	//
-	// Posts
-	//
-	e.GET("/api/posts/:id", posts.Get() )
-	e.POST("/api/posts", posts.Post() )
-	e.PUT("/api/posts", posts.Put() )
-	e.DELETE("/api/posts/:id", posts.Delete() )
-
-	//
-	// Signin
-	//
-	e.POST("/api/signin", signin.Post() )
-
-	//
-	// Websockets
-	//
-	e.GET( "/websocket", websock.Connect )
+  //
+  // Users
+  //
+  e.GET("/api/users/:id", users.Get() )
+  e.POST("/api/users", users.Create() )
+  e.PUT("/api/users", users.Update() )
+  e.DELETE("/api/users/:id", users.Delete() )
 
   //
-	// Ups
-	//
-	e.GET("/api/ups/user/:id", ups.Get() ) // Get a users ups
-	e.POST("/api/ups", ups.Post() ) // Get a users ups
-	e.DELETE("/api/ups/:id", ups.Delete() ) // Get a posts ups
+  // User connection
+  //
+
+  // NOTE: /:id will later be removed
+  //			 All connections should be returned
+  //			 /:id is now a replacement for logged in user id until sessions works.
+  e.GET("/api/user-connections/:id", userConnections.Get() )
+  e.POST("/api/user-connections", userConnections.Connect() )
+  e.PUT("/api/user-connections", userConnections.Accept() )
+
+  // NOTE: Change this to DELETE after sessions are working,
+  //			 change /remove to /:id
+  e.POST( "/api/user-connections/remove", userConnections.Disconnect() )
+  e.GET( "/api/my-contacts/:id", userConnections.MyContacts() )
+
+  //
+  // Articles
+  //
+  e.GET("/api/article/:id", article.Get() )
+  e.POST("/api/article", article.Post() )
+  e.PUT("/api/article", article.Put() )
+  e.DELETE("/api/article/:id", article.Delete() )
+
+  //
+  // Posts
+  //
+  e.GET("/api/posts/:id", posts.Get() )
+  e.POST("/api/posts", posts.Post() )
+  e.PUT("/api/posts", posts.Put() )
+  e.DELETE("/api/posts/:id", posts.Delete() )
+
+  //
+  // Signin
+  //
+  e.POST("/api/signin", signin.Post() )
+
+  //
+  // Websockets
+  //
+  e.GET( "/websocket", websock.Connect )
+
+  //
+  // Ups
+  //
+  e.GET("/api/ups/user/:id", ups.Get() ) // Get a users ups
+  e.POST("/api/ups", ups.Post() ) // Get a users ups
+  e.DELETE("/api/ups/:id", ups.Delete() ) // Get a posts ups
 
 }
