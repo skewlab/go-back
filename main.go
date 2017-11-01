cf19963cd80114455af18e84f6968a8b3c4508fa
fippli <filip.carl.johansson@gmail.com>
2017-11-01 22:03:37 +0100

    Fixed dev port setting bug

--- main.go
+++ main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-	"encoding/json"
+  "encoding/json"
 
-	"./api"
-	"./util"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+  "./api"
+  "./util"
+  "github.com/labstack/echo"
+  "github.com/labstack/echo/middleware"
 )
 
 /*
-	Config struct
-	Map ./config.json to this struct.
+  Config struct
+  Map ./config.json to this struct.
 */
 type Config struct {
-	Port   string
-	Static string
+  Port            string
+  Static          string
+  FrontEndDevPort string
 }
 
 func main() {
 
-	var config Config
-	configFile := util.ReadFile("config.json")
-	json.Unmarshal([]byte(configFile), &config)
-	e := echo.New()
+  var config Config
+  configFile := util.ReadFile("config.json")
+  json.Unmarshal([]byte(configFile), &config)
+  e := echo.New()
 
-	// NOTE: Allow CORS for development
-	// This should be carefully set in production mode
-	//e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"localhost", "http://localhost:3001", "127.0.0.1"},
-		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
-		AllowCredentials: true,
-	}))
-	///////////////
+  // NOTE: Allow CORS for development
+  // This should be carefully set in production mode
+  //e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+  e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+    AllowOrigins:     []string{"localhost", config.FrontEndDevPort},
+    AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+    AllowCredentials: true,
+  }))
+  ///////////////
 
-	// Static routes for main page and manage page
-	e.Use(middleware.Static(config.Static))
+  // Static routes for main page and manage page
+  e.Use(middleware.Static(config.Static))
 
-	api.Module(e)
+  api.Module(e)
 
-	e.Start(config.Port)
+  e.Start(config.Port)
 
 }
