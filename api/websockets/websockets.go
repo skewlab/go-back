3697f6c996fe9b58f7b0bf8442c24b971b4ab46d
jonas <jonasj9@kth.se>
2018-02-03 12:09:49 +0100

    notification works, need to send data back to client in the right format

--- api/websockets/websockets.go
+++ api/websockets/websockets.go
@@ -9,42 +9,95 @@ Most of the code is taken from:
 https://echo.labstack.com/cookbook/websocket
 */
 
-
 package websock
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"time"
+
+	"../../util"
+
 	"github.com/labstack/echo"
+	"github.com/lib/pq"
 	"golang.org/x/net/websocket"
 )
 
-func Connect( c echo.Context ) error {
+func waitForNotification(l *pq.Listener) []byte {
+	for {
+		select {
+		case n := <-l.Notify:
+			fmt.Println("Received data from channel [", n.Channel, "] :")
+			// Prepare notification payload for pretty print
+			var prettyJSON bytes.Buffer
+			err := json.Indent(&prettyJSON, []byte(n.Extra), "", "\t")
+			if err != nil {
+				fmt.Println("Error processing JSON: ", err)
+				return prettyJSON.Bytes()
+			}
+			fmt.Println(string(prettyJSON.Bytes()))
+			return prettyJSON.Bytes()
+		case <-time.After(90 * time.Second):
+			fmt.Println("Received no events for 90 seconds, checking connection")
+			go func() {
+				l.Ping()
+			}()
 
-	websocket.Handler( func( ws *websocket.Conn ) {
+		}
+	}
+}
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Dbname   string
+}
+
+func Connect(c echo.Context) error {
+	var DBconfig DBConfig
+	DBconfigFile := util.ReadFile("database/db.config.json")
+	err := json.Unmarshal([]byte(DBconfigFile), &DBconfig)
+
+	websocket.Handler(func(ws *websocket.Conn) {
 
 		defer ws.Close()
+		reportProblem := func(ev pq.ListenerEventType, err error) {
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}
+
+		listener := pq.NewListener("dbname="+DBconfig.Dbname+" user="+DBconfig.User+" password="+DBconfig.Password+" sslmode=disable", 10*time.Second, time.Minute, reportProblem)
+		err = listener.Listen("events")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Start monitoring PostgreSQL...")
 
 		for {
+			data := waitForNotification(listener)
+			// Write
+			fmt.Println(data)
+			err := websocket.Message.Send(ws, data)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+
 			// Read
 			msg := ""
-			err := websocket.Message.Receive( ws, &msg )
-
+			err = websocket.Message.Receive(ws, &msg)
 			if err != nil {
-				c.Logger().Error( err )
+				c.Logger().Error(err)
 			}
-
-			// Write
-			err = websocket.Message.Send( ws, "Hello, Client!" )
-
-			if err != nil {
-				c.Logger().Error( err )
-			}
-
-			fmt.Printf( "%s\n", msg )
+			fmt.Printf("%s\n", msg)
 
 		}
 
-	}).ServeHTTP( c.Response(), c.Request() )
+	}).ServeHTTP(c.Response(), c.Request())
 
 	return nil
 
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"./api"
 	"./util"
+	"github.com/coussej/pgbroadcast"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -22,7 +23,43 @@ type Config struct {
 	FrontEndDevPort string
 }
 
+// func waitForNotification(l *pq.Listener) {
+// 	for {
+// 		select {
+// 		case n := <-l.Notify:
+// 			fmt.Println("Received data from channel [", n.Channel, "] :")
+// 			// Prepare notification payload for pretty print
+// 			var prettyJSON bytes.Buffer
+// 			err := json.Indent(&prettyJSON, []byte(n.Extra), "", "\t")
+// 			if err != nil {
+// 				fmt.Println("Error processing JSON: ", err)
+// 				return
+// 			}
+// 			fmt.Println(string(prettyJSON.Bytes()))
+// 			return
+// 		case <-time.After(90 * time.Second):
+// 			fmt.Println("Received no events for 90 seconds, checking connection")
+// 			go func() {
+// 				l.Ping()
+// 			}()
+// 			return
+// 		}
+// 	}
+// }
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Dbname   string
+}
+
 func main() {
+
+	var DBconfig DBConfig
+	DBconfigFile := util.ReadFile("database/db.config.json")
+	err := json.Unmarshal([]byte(DBconfigFile), &DBconfig)
 
 	var config Config
 	configFile := util.ReadFile("config.json")
@@ -33,6 +70,31 @@ func main() {
 	database.DB = database.Connection()
 	fmt.Print("db done")
 	fmt.Print(database.DB)
+
+	// Create a new broadcaster
+	pb, err := pgbroadcaster.NewPgBroadcaster("dbname=" + DBconfig.Dbname + " user=" + DBconfig.User + " password=" + DBconfig.Password + " sslmode=disable")
+
+	// listen to the events channel
+	err = pb.Listen("events")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// reportProblem := func(ev pq.ListenerEventType, err error) {
+	// 	if err != nil {
+	// 		fmt.Println(err.Error())
+	// 	}
+	// }
+
+	// listener := pq.NewListener("dbname="+DBconfig.Dbname+" user="+DBconfig.User+" password="+DBconfig.Password+" sslmode=disable", 10*time.Second, time.Minute, reportProblem)
+	// err = listener.Listen("feed")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("Start monitoring PostgreSQL...")
+	// for {
+	// 	waitForNotification(listener)
+	// }
 
 	// NOTE: Allow CORS for development
 	// This should be carefully set in production mode
@@ -49,7 +111,5 @@ func main() {
 	e.Use(middleware.Static(config.Static))
 
 	api.Module(e)
-
 	e.Start(config.Port)
-
 }
