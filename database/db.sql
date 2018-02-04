23e7ab243edd35dba71831f95b4709ae35d7b609
jonas <jonasj9@kth.se>
2018-02-04 20:03:01 +0100

    added SQL Query to db

--- database/db.sql
+++ database/db.sql
@@ -119,3 +119,46 @@ CREATE TABLE Ups (
 
 -- --------------------------------------------------------
 create index alias_gin on users using gin(alias gin_trgm_ops);
+
+--
+-- Comments:
+-- add a trigger function that notifies if any posts change
+
+-- --------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION notify_event() RETURNS TRIGGER AS $$
+
+    DECLARE 
+        data json;
+        notification json;
+    
+    BEGIN
+    
+        -- Convert the old or new row to JSON, based on the kind of action.
+        -- Action = DELETE?             -> OLD row
+        -- Action = INSERT or UPDATE?   -> NEW row
+        IF (TG_OP = 'DELETE') THEN
+            data = row_to_json(OLD);
+        ELSE
+            data = row_to_json(NEW);
+        END IF;
+        
+        -- Contruct the notification as a JSON string.
+        notification = json_build_object(
+                          'table',TG_TABLE_NAME,
+                          'action', TG_OP,
+                          'data', data);
+        
+                        
+        -- Execute pg_notify(channel, notification)
+        PERFORM pg_notify('events',notification::text);
+        
+        -- Result is ignored since this is an AFTER trigger
+        RETURN NULL; 
+    END;
+    
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER posts_notify_event
+AFTER INSERT OR UPDATE OR DELETE ON posts
+    FOR EACH ROW EXECUTE PROCEDURE notify_event();
